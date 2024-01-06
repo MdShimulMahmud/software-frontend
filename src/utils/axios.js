60882eac83058d8d54a59ccb20e5203de7ca7170
MdShimulMahmud <shimulmahmud18@gmail.com>
2024-01-06 22:30:41 +0600

    last from my side

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://rentspot-j45m.onrender.com",
   withCredentials: true,
 });
 export default axiosInstance;
